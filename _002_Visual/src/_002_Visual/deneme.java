28707bca338d3a566d5d70f91ddef8a55f79ce3e
Onur ALACA <onuralcaa@gmail.com>
2024-03-31 12:45:28 +0300

    Image added

--- _002_Visual/src/_002_Visual/deneme.java
+++ _002_Visual/src/_002_Visual/deneme.java
@@ -2,9 +2,11 @@ package _002_Visual;
 
 import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.Image;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -37,9 +39,9 @@ public class deneme {
 		
 		jf.getContentPane().add(lbl1);
 		
+		Image img1 = new ImageIcon(deneme.class.getResource("/image/lblimage.png")).getImage();
 		
-		
-		
+		lbl1.setIcon(new ImageIcon(img1));
 		
 		
 		
