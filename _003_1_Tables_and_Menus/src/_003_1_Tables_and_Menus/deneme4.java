d56878b8ee2ffaf03a62964b5195f25fde4b7405
Onur ALACA <onuralcaa@gmail.com>
2024-04-04 16:16:02 +0300

    Visiable error

--- _003_1_Tables_and_Menus/src/_003_1_Tables_and_Menus/deneme4.java
+++ _003_1_Tables_and_Menus/src/_003_1_Tables_and_Menus/deneme4.java
@@ -11,6 +11,8 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
 
 public class deneme4 {
@@ -18,11 +20,27 @@ public class deneme4 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		JFrame jf = new JFrame();
+		JFrame jf = new JFrame("Table example");
 		jf.setSize(500, 600);
 		jf.setLocation(100, 200);
 		
+		String [][] data = { 
+				{"1", "Onur", "Osmaniye"}, 
+				{"2", "Betul", "Kahramanmaraş"},
+				{"3", "Osman", "Ankara"}
+				
+		};
 		
+		String [][] headers = { 
+				{"Numara", "İsim", "Memleket"}, 
+		}; 
+		
+		JTable table = new JTable(data, headers);
+		JScrollPane sp = new JScrollPane(table);
+		jf.add(sp);
+		
+		
+		jf.setLayout(null);
 		
 		jf.setVisible(true);
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
