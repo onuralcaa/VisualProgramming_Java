b705005d88ff0438e33ae5433c96f5bc41fccba9
Onur ALACA <onuralcaa@gmail.com>
2024-04-04 16:09:04 +0300

    New File created

---
+++ _003_1_Tables_and_Menus/src/_003_1_Tables_and_Menus/deneme4.java
@@ -0,0 +1,32 @@
+package _003_1_Tables_and_Menus;
+
+
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Image;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+
+public class deneme4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		JFrame jf = new JFrame();
+		jf.setSize(500, 600);
+		jf.setLocation(100, 200);
+		
+		
+		
+		jf.setVisible(true);
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
+
+}
---
+++ _003_1_Tables_and_Menus/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module _003_1_Tables_and_Menus {
+	requires java.desktop;
+}
