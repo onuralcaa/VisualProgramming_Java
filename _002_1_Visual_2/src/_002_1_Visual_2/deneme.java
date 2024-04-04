485d056e3d6d166fc0245fee63dcabb983e7d80a
Onur ALACA <onuralcaa@gmail.com>
2024-04-04 15:55:24 +0300

    some more components

--- _002_1_Visual_2/src/_002_1_Visual_2/deneme.java
+++ _002_1_Visual_2/src/_002_1_Visual_2/deneme.java
@@ -13,8 +13,10 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPasswordField;
+import javax.swing.JRadioButton;
 import javax.swing.JTextArea;
 import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 
 public class deneme {
 
@@ -31,6 +33,14 @@ public class deneme {
 		JCheckBox cbH = new JCheckBox("Hayır");
 		cbE.setBounds(200, 200, 50, 50);//sizing process
 		
+		JRadioButton rb1 = new JRadioButton("Erkek");
+		
+		//ComboBox a eleman ekleme
+		String str[] = {"C", "C++", "Java", "PHP"};
+		
+		JComboBox cb1 = new JComboBox(str);
+		
+		
 		//Hangi checkbox ın seçildiğini anlamak
 		JButton btn1 = new JButton("Selected");
 		
@@ -40,22 +50,17 @@ public class deneme {
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
 				
-				if(cbE.isSelected())
-				{
-					System.out.println("Yes, selected."); //Konsol kullanma
-				}
-				
-				if(cbH.isSelected())
-				{
-					System.out.println("No, selected.");
-				}
+				String yaz = "Selected programming language "+cb1.getSelectedItem();
+				System.out.println(yaz);
 			}
 		});
 		
-		
+		//ögeleri container a dahil etme 
 		jf.getContentPane().add(cbE); 
 		jf.getContentPane().add(cbH); 
 		jf.getContentPane().add(btn1);
+		jf.getContentPane().add(rb1);
+		jf.getContentPane().add(cb1);
 		
 		jf.setVisible(true);
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
