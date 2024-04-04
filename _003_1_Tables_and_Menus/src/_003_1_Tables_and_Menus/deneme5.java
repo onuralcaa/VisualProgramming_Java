9acd41a6ac08d2cd30d70c9546d78f4b1fb65252
Onur ALACA <onuralcaa@gmail.com>
2024-04-04 16:51:15 +0300

    Menu and submenu

--- _003_1_Tables_and_Menus/src/_003_1_Tables_and_Menus/deneme5.java
+++ _003_1_Tables_and_Menus/src/_003_1_Tables_and_Menus/deneme5.java
@@ -11,6 +11,9 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
@@ -23,9 +26,33 @@ public class deneme5 {
 		jf.setSize(500, 600);
 		jf.setLocation(100, 200);
 		
+		JMenuBar mb = new JMenuBar();
+		JMenu menu = new JMenu("File");
+		JMenu submenu = new JMenu("Submenu");
+		
+		//menu ekleme
+		JMenuItem i1 = new JMenuItem("File");
+		JMenuItem i2 = new JMenuItem("Open File");
+		JMenuItem i3 = new JMenuItem("Recent Files");
+		JMenuItem i4 = new JMenuItem("Close Editor");
+		
+		//alt menuler 
+		JMenuItem a1 = new JMenuItem("Java Project");
+		JMenuItem a2 = new JMenuItem("Maven Project");
+		
+		submenu.add(a1);
+		submenu.add(a2);
+		
+		menu.add(i1);
+		menu.add(i2);
+		menu.add(i3);
+		menu.add(i4);
+
+		
+	
+
 		
 		jf.setLayout(new FlowLayout());
-		
 		jf.setVisible(true);
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
