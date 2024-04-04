37cc6d029ce8025f9105c77db8b5d9ecaddac2f0
Onur ALACA <onuralcaa@gmail.com>
2024-04-04 16:44:04 +0300

    Menu file created

--- _003_1_Tables_and_Menus/src/_003_1_Tables_and_Menus/deneme5.java
+++ _003_1_Tables_and_Menus/src/_003_1_Tables_and_Menus/deneme5.java
@@ -15,27 +15,13 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
 
-public class deneme4 {
+public class deneme5 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		JFrame jf = new JFrame("Table example");
 		jf.setSize(500, 600);
 		jf.setLocation(100, 200);
-		
-		String [][] data = { 
-				{"1", "Onur", "Osmaniye"}, 
-				{"2", "Betul", "Kahramanmaraş"},
-				{"3", "Osman", "Ankara"}
-				
-		};
-		
-		String []headers = { "Numara", "İsim", "Memleket" }; 
-		
-		JTable table = new JTable(data, headers);
-		
-		JScrollPane sp = new JScrollPane(table);
-		jf.add(sp);
 		
 		
 		jf.setLayout(new FlowLayout());
