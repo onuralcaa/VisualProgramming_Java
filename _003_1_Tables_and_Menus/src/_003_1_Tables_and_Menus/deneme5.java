dddca24230b630e9110be25cd92b15417bc66da2
Onur ALACA <onuralcaa@gmail.com>
2024-04-04 16:58:09 +0300

    Menu click process

--- _003_1_Tables_and_Menus/src/_003_1_Tables_and_Menus/deneme5.java
+++ _003_1_Tables_and_Menus/src/_003_1_Tables_and_Menus/deneme5.java
@@ -4,6 +4,8 @@ package _003_1_Tables_and_Menus;
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
@@ -14,6 +16,7 @@ import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
@@ -28,10 +31,9 @@ public class deneme5 {
 		
 		JMenuBar mb = new JMenuBar();
 		JMenu menu = new JMenu("File");
-		JMenu submenu = new JMenu("Submenu");
+		JMenu submenu = new JMenu("New");
 		
 		//menu ekleme
-		JMenuItem i1 = new JMenuItem("File");
 		JMenuItem i2 = new JMenuItem("Open File");
 		JMenuItem i3 = new JMenuItem("Recent Files");
 		JMenuItem i4 = new JMenuItem("Close Editor");
@@ -43,7 +45,7 @@ public class deneme5 {
 		submenu.add(a1);
 		submenu.add(a2);
 		
-		menu.add(i1);
+
 		menu.add(i2);
 		menu.add(i3);
 		menu.add(i4);
@@ -53,7 +55,16 @@ public class deneme5 {
 		
 		jf.setJMenuBar(mb); //son aşama
 	
-
+		
+		i2.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				
+				JOptionPane.showMessageDialog(i2, "Clicked the menu");
+			}
+		});
 		
 		jf.setLayout(new FlowLayout());
 		jf.setVisible(true);
