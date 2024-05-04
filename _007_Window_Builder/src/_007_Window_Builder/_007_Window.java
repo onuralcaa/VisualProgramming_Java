627f2116bae8a1519bd40c08ba30f20a439165f4
Onur ALACA <onuralcaa@gmail.com>
2024-05-04 23:47:11 +0300

    Window created.

--- _007_Window_Builder/src/_007_Window_Builder/_007_Window.java
+++ _007_Window_Builder/src/_007_Window_Builder/_007_Window.java
@@ -12,13 +12,14 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.Font;
 
 public class _007_Window extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JTextField textSifre;
-	private JTextField textName;
+	private JTextField textEkran;
 
 	/**
 	 * Launch the application.
@@ -35,46 +36,184 @@ public class _007_Window extends JFrame {
 			}
 		});
 	}
-
+	
+	
+	public void addInput(String str)
+	{
+		textEkran.setText(textEkran.getText() + str);
+	}
+	
+	
+	
 	/**
 	 * Create the frame.
 	 */
 	public _007_Window() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 702, 514);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		textName = new JTextField();
-		textName.setBounds(126, 65, 86, 17);
-		contentPane.add(textName);
-		textName.setColumns(10);
+		textEkran = new JTextField();
+		textEkran.setBounds(106, 20, 356, 49);
+		contentPane.add(textEkran);
+		textEkran.setColumns(10);
 		
-		JButton btnGiris = new JButton("Giriş");
-		btnGiris.setForeground(new Color(255, 0, 0));
-		btnGiris.setBackground(new Color(255, 255, 255));
-		btnGiris.addActionListener(new ActionListener() {
+		JLabel lblSonuc = new JLabel("SONUC");
+		lblSonuc.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblSonuc.setBounds(527, 20, 81, 49);
+		contentPane.add(lblSonuc);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(44, 90, 612, 379);
+		contentPane.add(panel);
+		panel.setLayout(new GridLayout(4, 4, 10, 10));
+		
+		JButton btn9 = new JButton("9");
+		btn9.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				addInput(e.getActionCommand()); //butonun degerini alma
 			}
 		});
-		btnGiris.setBounds(126, 140, 79, 19);
-		contentPane.add(btnGiris);
+		btn9.setFont(new Font("Tahoma", Font.PLAIN, 32));
+		panel.add(btn9);
 		
-		textSifre = new JTextField();
-		textSifre.setBounds(126, 93, 86, 17);
-		contentPane.add(textSifre);
-		textSifre.setColumns(10);
+		JButton btn8 = new JButton("8");
+		btn8.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				addInput(e.getActionCommand()); //butonun degerini alma
+			}
+		});
+		btn8.setFont(new Font("Tahoma", Font.PLAIN, 32));
+		panel.add(btn8);
 		
-		JLabel lblNewLabel = new JLabel("Kullanıcı adı");
-		lblNewLabel.setBounds(44, 65, 62, 14);
-		contentPane.add(lblNewLabel);
+		JButton btn7 = new JButton("7");
+		btn7.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				addInput(e.getActionCommand()); //butonun degerini alma
+			}
+		});
+		btn7.setFont(new Font("Tahoma", Font.PLAIN, 32));
+		panel.add(btn7);
 		
-		JLabel lblNewLabel_1 = new JLabel("Şifre");
-		lblNewLabel_1.setBounds(44, 93, 62, 14);
-		contentPane.add(lblNewLabel_1);
+		JButton btnTopla = new JButton("+");
+		btnTopla.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				addInput(e.getActionCommand()); //butonun degerini alma
+			}
+		});
+		btnTopla.setFont(new Font("Tahoma", Font.PLAIN, 32));
+		panel.add(btnTopla);
+		
+		JButton btn6 = new JButton("6");
+		btn6.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				addInput(e.getActionCommand()); //butonun degerini alma
+			}
+		});
+		btn6.setFont(new Font("Tahoma", Font.PLAIN, 32));
+		panel.add(btn6);
+		
+		JButton btn5 = new JButton("5");
+		btn5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				addInput(e.getActionCommand()); //butonun degerini alma
+			}
+		});
+		btn5.setFont(new Font("Tahoma", Font.PLAIN, 32));
+		panel.add(btn5);
+		
+		JButton btn4 = new JButton("4");
+		btn4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				addInput(e.getActionCommand()); //butonun degerini alma
+			}
+		});
+		btn4.setFont(new Font("Tahoma", Font.PLAIN, 32));
+		panel.add(btn4);
+		
+		JButton btnCikar = new JButton("-");
+		btnCikar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				addInput(e.getActionCommand()); //butonun degerini alma
+			}
+		});
+		btnCikar.setFont(new Font("Tahoma", Font.PLAIN, 32));
+		panel.add(btnCikar);
+		
+		JButton btn3 = new JButton("3");
+		btn3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				addInput(e.getActionCommand()); //butonun degerini alma
+			}
+		});
+		btn3.setFont(new Font("Tahoma", Font.PLAIN, 32));
+		panel.add(btn3);
+		
+		JButton btn2 = new JButton("2");
+		btn2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				addInput(e.getActionCommand()); //butonun degerini alma
+			}
+		});
+		btn2.setFont(new Font("Tahoma", Font.PLAIN, 32));
+		panel.add(btn2);
+		
+		JButton btn1 = new JButton("1");
+		btn1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				addInput(e.getActionCommand()); //butonun degerini alma
+			}
+		});
+		btn1.setFont(new Font("Tahoma", Font.PLAIN, 32));
+		panel.add(btn1);
+		
+		JButton btnBol = new JButton("/");
+		btnBol.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				addInput(e.getActionCommand()); //butonun degerini alma
+			}
+		});
+		btnBol.setFont(new Font("Tahoma", Font.PLAIN, 32));
+		panel.add(btnBol);
+		
+		JButton btn0 = new JButton("0");
+		btn0.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				addInput(e.getActionCommand()); //butonun degerini alma
+			}
+		});
+		btn0.setFont(new Font("Tahoma", Font.PLAIN, 32));
+		panel.add(btn0);
+		
+		JButton btnClear = new JButton("CLEAR");
+		btnClear.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textEkran.setText("  ");//ekran temizleme
+			}
+		});
+		btnClear.setFont(new Font("Tahoma", Font.PLAIN, 32));
+		panel.add(btnClear);
+		
+		JButton btnSonuc = new JButton("=");
+		btnSonuc.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				addInput(e.getActionCommand()); //butonun degerini alma
+			}
+		});
+		btnSonuc.setFont(new Font("Tahoma", Font.PLAIN, 32));
+		panel.add(btnSonuc);
+		
+		JButton btnCarp = new JButton("X");
+		btnCarp.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				addInput(e.getActionCommand()); //butonun degerini alma
+			}
+		});
+		btnCarp.setFont(new Font("Tahoma", Font.PLAIN, 32));
+		panel.add(btnCarp);
 	}
-
 }
